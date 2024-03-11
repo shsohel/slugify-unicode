25a2adcfce7fe8712c2ea90f3e76bb1f793d4cb8
SH SOHEL <49935124+shsohel@users.noreply.github.com>
2024-03-11 10:29:47 +0000

    update data

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function slugify(text) {
   return text
-    .toLowerCase()
-    .replace(text, text)
-    .replace(/^-+|-+$/g, '')
-    .replace(/\s/g, '-')
-    .replace(/\-\-+/g, '-');
+  .toLowerCase()
+  .replace(/[^\w\s-]/g, '') // replace special characters except hyphen and whitespace
+  .replace(/\.$/, '')      // remove last dot
+  .replace(/^-+|-+$/g, '') // remove leading/trailing hyphens
+  .replace(/\s+/g, '-')    // replace whitespace with hyphens
+  .replace(/-{2,}/g, '-'); // replace multiple consecutive hyphens with single hyphen
 }
 
 module.exports.slugify = slugify;
