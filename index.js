4a9a405eec2c723ee3db685e870e71084a67fa34
SH Sohel <shsohel.tc@gmail.com>
2024-03-17 14:04:15 +0600

    fdfdf

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function slugify(text) {
   return text
-  .toLowerCase()
-  .replace(/[^\w\s-]/g, '') // replace special characters except hyphen and whitespace
-  .replace(/\.$/, '')      // remove last dot
-  .replace(/^-+|-+$/g, '') // remove leading/trailing hyphens
-  .replace(/\s+/g, '-')    // replace whitespace with hyphens
-  .replace(/-{2,}/g, '-'); // replace multiple consecutive hyphens with single hyphen
+    .toLowerCase()
+    .replace(/[^\p{L}\p{N}\s-]/gu, "") // replace non-letter, non-number, non-space, and non-hyphen characters
+    .replace(/\.$/, "") // remove last dot
+    .replace(/^-+|-+$/gu, "") // remove leading/trailing hyphens
+    .replace(/\s+/gu, "-") // replace whitespace with hyphens
+    .replace(/-{2,}/gu, "-"); // replace multiple consecutive hyphens with single hyphen
 }
-
 module.exports.slugify = slugify;
